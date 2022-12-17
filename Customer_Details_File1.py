08a9d4ab73fea533c36fa1bab431fe4537b1a3a5
ShashiSable <shsable84@gmail.com>
2022-12-17 11:08:43 +0530

    Adding 1st file@Customer_Details

---
+++ Customer_Details_File1.py
@@ -0,0 +1,12 @@
+#
+#
+# <!DOCTYPE html>
+# <html lang="en">
+#   <head>
+#     <title>This is my first website</title>
+#   </head>
+#   <body>
+#     <p>This is a paragraph.</p>
+# 	<p>This is another paragraph.</p>
+#   </body>
+# </html>
